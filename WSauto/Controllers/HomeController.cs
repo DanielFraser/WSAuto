8e4aace8b301a401b99d77dfdf1d8eb611f82c74
Daniel R Fraser <dukeydk@gmail.com>
2019-02-18 22:50:44 -0500

    Initial Commit

---
+++ WSauto/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WSauto.ProdReference;
+
+namespace WSauto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+
+        public JsonResult getProducts(string prefix)
+        {
+            ProductsSoapClient psc = new ProductsSoapClient();
+            return Json(psc.names(prefix), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
